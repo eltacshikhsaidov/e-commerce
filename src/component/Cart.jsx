cf666a3c9181a41a32220491d8c3b350ebde3607
eltacshikhsaidov <eltac.shixseyidov250301@gmail.com>
2022-05-18 14:29:52 +0400

    fixing some warnings

--- src/component/Cart.jsx
+++ src/component/Cart.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { deleteProductFromCart } from '../redux/action/index';
+// import { useSelector, useDispatch } from 'react-redux';
+// import { deleteProductFromCart } from '../redux/action/index';
 
 const Cart = () => {
 
-    const state = useSelector((state) => state.addProductToCart);
-    const dispatch = useDispatch();
+    // const state = useSelector((state) => state.addProductToCart);
+    // const dispatch = useDispatch();
 
-    const cartProducts = (cartProduct) => {
-        <div className="px-4 my-5 bg-light">
-            <div className="container py-4">
-                <button className="btn-close float-end" aria-label='Close'></button>
-                <div className="row justify-content-center">
-                    <div className="col-md-4">
-                        <h3>test</h3>
-                        <p></p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    }
+    // const cartProducts = (cartProduct) => {
+    //     <div className="px-4 my-5 bg-light">
+    //         <div className="container py-4">
+    //             <button className="btn-close float-end" aria-label='Close'></button>
+    //             <div className="row justify-content-center">
+    //                 <div className="col-md-4">
+    //                     <h3>test</h3>
+    //                     <p></p>
+    //                 </div>
+    //             </div>
+    //         </div>
+    //     </div>
+    // }
 
 
     return (
