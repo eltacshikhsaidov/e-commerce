4e31268397ac6fef522c17de0914ac213eba1782
suay-selay <suay_999@hotmail.com>
2022-06-02 19:54:13 +0400

    fixing bootstrap errors

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
