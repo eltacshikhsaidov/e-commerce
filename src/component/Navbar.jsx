8d5f622ec5b4dd0671dc7072f6a8233b484c0296
suay-selay <suay_999@hotmail.com>
2022-06-03 14:45:35 +0400

    changing favicon

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -11,50 +11,45 @@ const Navbar = () => {
 
     return (
         <div>
-            <nav className="navbar navbar-expand-lg bg-white py-3 shadow-sm">
-                <div className="container">
-                    <NavLink className="navbar-brand fw-bold fs-4" to="/">
-                        E-Commerce
-                    </NavLink>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon">
-                            {/* toggle icon */}
-                            <i className="fa fa-bars mt-2"></i>
-                        </span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <NavLink className="nav-link active" aria-current="page" to="/">Home</NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink className="nav-link" to="products">Products</NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink className="nav-link" to="about">About</NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink className="nav-link" to="contact">Contact</NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink className="nav-link" to="dashboard">Dashboard</NavLink>
-                            </li>
-                        </ul>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light bg-white py-3 shadow-sm">
 
+                {/* <a className="navbar-brand" href="#">eStore</a> */}
+                <NavLink className={'navbar-brand'} to='/'>
+                    <img src={'/assets/logo.png'} alt="Navbar Logo" width='100px' height='50px' />
+                </NavLink>
 
-                        <div className="buttons">
-                            <NavLink to="/login" className="btn btn-outline-dark">
-                                <i className="fa fa-sign-in me-1"></i> Login
-                            </NavLink>
-                            <NavLink to="/register" className="btn btn-outline-dark ms-2">
-                                <i className="fa fa-user-plus me-1"></i> Register
-                            </NavLink>
-                            <NavLink to="/cart" className="btn btn-outline-dark ms-2">
-                                <i className="fa fa-shopping-cart me-1"></i> Cart ({state.length})
-                            </NavLink>
-                        </div>
+                <button className="navbar-toggler mx-3" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse px-3" id="navbarNav">
+                    <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <NavLink className="nav-link active" aria-current="page" to="/">Home</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="products">Products</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="about">About</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="contact">Contact</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="dashboard">Dashboard</NavLink>
+                        </li>
+                    </ul>
 
-
+                    <div className="buttons">
+                        <NavLink to="/login" className="btn btn-outline-dark">
+                            <i className="fa fa-sign-in me-1"></i> Login
+                        </NavLink>
+                        <NavLink to="/register" className="btn btn-outline-dark ms-2">
+                            <i className="fa fa-user-plus me-1"></i> Register
+                        </NavLink>
+                        <NavLink to="/cart" className="btn btn-outline-dark ms-2">
+                            <i className="fa fa-shopping-cart me-1"></i> Cart ({state.length})
+                        </NavLink>
                     </div>
                 </div>
             </nav>
--- src/component/products/Products.jsx
+++ src/component/products/Products.jsx
@@ -40,7 +40,7 @@ const Products = () => {
 
     return (
         <div>
-            <div className="container my-5 py-5">
+            <div className="container my-1 py-4">
                 <div className="row">
                     <div className="col-12 mb-5">
                         <h1 className="display-6 fw-bolder text-center">
